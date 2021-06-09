2b09fc6f3431f7b52073ab85655fe59d7c082f24
Shir Dekel <shirdekel@yahoo.com.au>
2021-06-09 19:02:36 +1000

    Add front matter dependency

--- R/get_thesis_rmd.R
+++ R/get_thesis_rmd.R
@@ -6,10 +6,15 @@
 ##' @author Shir Dekel
 ##' @export
 get_thesis_deps <- function() {
+  front_matter <-
+    file.path("rmd", "front_matter") %>%
+    list.files(full.names = TRUE)
+
   rmd <-
     file.path("_bookdown.yml") %>%
     yaml::read_yaml() %>%
-    pluck("rmd_files")
+    pluck("rmd_files") %>%
+    c(front_matter)
 
   templates <-
     file.path("templates") %>%
