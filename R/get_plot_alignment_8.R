a45dfbf3661c4686c541aca77ad8da3af4b13d8a
Shir Dekel <shirdekel@yahoo.com.au>
2021-06-23 09:11:44 +1000

    Tidy

--- R/get_plot_alignment_8.R
+++ R/get_plot_alignment_8.R
@@ -3,9 +3,9 @@
 ##' @return
 ##' @author Shir Dekel
 ##' @export
-get_plot_alignment_8 <- function(data_clean_alignment_8) {
+get_plot_alignment_8 <- function(data = alignment8::data) {
   data_clean_filtered <-
-    data_clean_alignment_8 %>%
+    data %>%
     nest_by(
       id,
       reliability_amount,
@@ -20,7 +20,6 @@ get_plot_alignment_8 <- function(data_clean_alignment_8) {
     data_clean_filtered %>%
     get_omnibus_alignment_8("allocation") %>%
     afex_plot_alignment_8(dv_label = "Allocation (%)")
-
 
   ranking <-
     data_clean_filtered %>%
