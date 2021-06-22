6fabc8292d07c2c80eefb8269c72524c276dcbf2
Shir Dekel <shirdekel@yahoo.com.au>
2021-06-23 09:05:26 +1000

    Fix analysis
    - To allow for both forecast measures.

--- R/get_omnibus_alignment_4.R
+++ R/get_omnibus_alignment_4.R
@@ -1,16 +1,17 @@
 #' @title Omnibus
-##' @param dv
+#' @param data
+#' @param dv
 #' @return
 #' @author Shir Dekel
 #' @export
-get_omnibus_alignment_4 <- function(data_clean_alignment_4, dv) {
-  data_clean_alignment_4 %>%
+get_omnibus_alignment_4 <- function(data, dv) {
+  data %>%
     ## Makes sure that npv levels are in the correct order for the emmeans
     ## linear trend analysis
     arrange(npv_amount) %>%
     aov_ez(
       id = "id",
-      dv = "forecast_mean",
+      dv = dv,
       between = c("alignment", "reliability_amount"),
       within = "npv_amount",
       data = .,
