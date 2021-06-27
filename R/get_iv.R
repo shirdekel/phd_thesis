3b57f550c452fc21a7a460a877e5113e3ce0a660
Shir Dekel <shirdekel@yahoo.com.au>
2021-06-28 09:50:04 +1000

    Clarify IVs

--- R/get_iv.R
+++ R/get_iv.R
@@ -7,58 +7,72 @@
 #' @export
 get_iv <- function() {
   list(
+    ## Aggregation 1
     c(
       "similarity",
       "awareness",
       "presentation"
     ),
+    ## Aggregation 2
     c(
       "awareness",
       "distribution",
       "presentation"
     ),
+    ## Aggregation 3
     "similarity",
+    ## Aggregation 4
     "awareness",
+    ## Alignment 1
     c(
       "alignment",
       "reliability_amount"
     ),
+    ## Alignment 2
     c(
       "alignment",
       "reliability_amount",
       "npv_amount"
     ),
+    ## Alignment 3
     c(
       "alignment",
       "reliability_amount"
     ),
+    ## Alignment 4
     c(
       "alignment",
       "reliability_amount"
     ),
+    ## Alignment 5
     c(
       "alignment",
       "reliability_amount"
     ),
+    ## Alignment 6
     c(
       "hint",
       "variance"
     ),
+    ## Alignment 7
     c(
       "reliability_type",
       "reliability_amount",
       "npv_amount"
     ),
+    ## Alignment 8
     c(
       "alignment",
       "reliability_type",
       "reliability_amount",
       "npv_amount"
     ),
+    ## Anecdotes 1
     c(
       "anecdote",
       "alignment"
     ),
+    ## Anecdotes 2
     c(
       "anecdote_between",
       "similarity",
